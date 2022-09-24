d76e5b6031384c792a5383facd48ec22e160ff82
pat <patgiok@gmail.com>
2022-09-24 16:29:25 -0700

    basic ui structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,15 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className='text-red-300'>hello world</div>
+    <div className='h-screen w-screen flex flex-col justify-center items-center'>
+      <div className='text-2xl text-center'> Which Pokemon is rounder?</div>
+      <div className="p-3"></div>
+      <div className='border rounded p-8 flex items-center max-w-2xl justify-between'>
+        <div className='w-16 h-16 bg-red-700'></div>
+        <div className='p-8'>VS</div>
+        <div className='w-16 h-16 bg-red-600'></div>
+      </div>
+    </div>
   )
 }
 
