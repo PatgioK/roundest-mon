b952e98acbb9ce0a2ac74449105c31598a0fd967
pat <patgiok@gmail.com>
2022-09-24 20:44:47 -0700

    backend with pokemon data new router with pokemon client, get data, displayed sprite + name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from 'next'
 import { useEffect, useState } from 'react';
 
 
-export default function Home () {
+export default function Home() {
 
   // Default trpc test
   // const {data, isLoading} = trpc.useQuery(["hello", {text: 'pat'}])
@@ -12,26 +12,43 @@ export default function Home () {
   // if (data) return <div>{data.greeting}</div>
 
   // https://traviswimer.com/blog/easily-fix-react-hydration-errors/
-  const [hydrated, setHydrated] = useState(false);
-    // This forces a rerender, so the date is rendered
-		// the second time but not the first
-  useEffect(() => {		
-    setHydrated(true);
-  },[])
-  if (!hydrated){
-    return null;
-  }
+  // const [hydrated, setHydrated] = useState(false);
+  //   // This forces a rerender, so the date is rendered
+  // 	// the second time but not the first
+  // useEffect(() => {		
+  //   setHydrated(true);
+  // },[])
+  // if (!hydrated){
+  //   return null;
+  // }
 
-  const [first, second] = getOptionsForVote();
-  
+
+  const [ids, updateIds] = useState(getOptionsForVote);
+  const [first, second] = ids;
+
+  const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }]);
+  const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }]);
+  console.log(firstPokemon.data)
+
+  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+
+
+
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
       <div className='text-2xl text-center'> Which Pokemon is rounder?</div>
       <div className="p-3"></div>
       <div className='border rounded p-8 flex items-center max-w-2xl justify-between'>
-        <div className='w-16 h-16 bg-red-700'>{first}</div>
+        <div className='w-64 h-64 flex flex-col'>
+          <img src={firstPokemon.data?.sprites.front_default!} className='w-full' />
+          <div className='capitalize text-xl text-center'>{firstPokemon.data?.name}</div>
+        </div>
         <div className='p-8'>VS</div>
-        <div className='w-16 h-16 bg-red-600'>{second}</div>
+        <div className='w-64 h-64 flex flex-col'>
+          <img src={secondPokemon.data?.sprites.front_default!} className='w-full' />
+          <div className='capitalize text-xl text-center'>{secondPokemon.data?.name}</div>
+        </div>
+        <div className="p-4"></div>
       </div>
     </div>
   )
--- src/utils/GetRandomPokemon.ts
+++ src/utils/GetRandomPokemon.ts
@@ -1,5 +1,5 @@
 
-const MAX_DEX_ID = 5;
+const MAX_DEX_ID = 251;
 
 //params to skip if pokemon is already selected
 export const getRandomPokemon: (notThisOne?: number) => number = (
