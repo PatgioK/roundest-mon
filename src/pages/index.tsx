5f6f4ae2214f1526b17e0e6cdea5480924ad9018
pat <patgiok@gmail.com>
2022-09-24 18:23:49 -0700

    add randompokemon id fetcher, hydration error??

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,23 @@
+import { getOptionsForVote } from '@/utils/GetRandomPokemon'
 import { trpc } from '@/utils/trpc'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  const {data, isLoading} = trpc.useQuery(["hello", {text: 'pat'}])
+  // Default trpc test
+  // const {data, isLoading} = trpc.useQuery(["hello", {text: 'pat'}])
+  // if(isLoading) return <div>Loading...</div>
+  // if (data) return <div>{data.greeting}</div>
 
-  if(isLoading) return <div>Loading...</div>
-
-  if (data) return <div>{data.greeting}</div>
+  const [first, second] = getOptionsForVote();
   
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
       <div className='text-2xl text-center'> Which Pokemon is rounder?</div>
       <div className="p-3"></div>
       <div className='border rounded p-8 flex items-center max-w-2xl justify-between'>
-        <div className='w-16 h-16 bg-red-700'></div>
+        <div className='w-16 h-16 bg-red-700'>{first}</div>
         <div className='p-8'>VS</div>
-        <div className='w-16 h-16 bg-red-600'></div>
+        <div className='w-16 h-16 bg-red-600'>{second}</div>
       </div>
     </div>
   )
---
+++ src/utils/GetRandomPokemon.ts
@@ -0,0 +1,19 @@
+
+const MAX_DEX_ID = 5;
+
+//params to skip if pokemon is already selected
+export const getRandomPokemon: (notThisOne?: number) => number = (
+    notThisOne
+    ) => {
+    const pokedexNumber = Math.floor(Math.random() * (MAX_DEX_ID) + 1)
+
+    if(pokedexNumber !== notThisOne) return pokedexNumber;
+        return getRandomPokemon(notThisOne)
+}
+
+export const getOptionsForVote = () => {
+    const firstId = getRandomPokemon();
+    const secondId = getRandomPokemon(firstId);
+
+    return [firstId, secondId]
+}
